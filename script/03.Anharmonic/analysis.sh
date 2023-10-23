8fe66f79f2375373d5d97db5b4b05b8a9e97b505
Mariano Mollo <marianomollo@protonmail.ch>
2023-10-23 19:02:45 +0200

    Continua script per integrazione termodinamica

--- script/03.Anharmonic/analysis.sh
+++ script/03.Anharmonic/analysis.sh
@@ -6,14 +6,18 @@ fi
 # DONE Ricava U_0 da OUTCAR
 U0=$(grep xlambda 01/OUTCAR | head -n 1 | awk '{print $6}')
 echo "$U0"
-# TODO Raccogli valori di OSZICAR
+# DONE Raccogli valori di OSZICAR
+if [ -f analysis/df ]; then
+    cat >analysis/df <<EOF
+# xlambda <dF-U0>
+EOF
+fi
 for folder in 01 02 03 04; do
     xlambda=$(grep xlambda $folder/OUTCAR | head -n 1 | awk '{print $3}')
     grep T= $folder/OSZICAR >"analysis/t.$xlambda"
     awk 'END{print NR}' "analysis/t.$xlambda"
-    awk -v u="$U0" '{a+=$7+u}END{print a/NR/64}' "analysis/t.$xlambda" >"analysis/l.$xlambda"
+    awk -v u="$U0" -v x="$xlambda" '{a+=$7+u}END{print x, a/NR/64}' "analysis/t.$xlambda" >>"analysis/df"
 done
-cat analysis/l* >analysis/df
-# TODO Calcola la media dell'energia di Helmholtz
-# TODO Raccogli le medie per ogni lambda in un file
+# DONE Calcola la media dell'energia di Helmholtz
+# DONE Raccogli le medie per ogni lambda in un file
 # TODO Integra per ottenere il volume corretto
