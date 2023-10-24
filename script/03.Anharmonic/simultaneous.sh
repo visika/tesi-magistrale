5fdec191053e70194a9c13fa37a21adc1e6524bc
Mariano Mollo <marianomollo@protonmail.ch>
2023-10-24 16:20:37 +0200

    Aggiorna script dinamica molecolare

--- script/03.Anharmonic/simultaneous.sh
+++ script/03.Anharmonic/simultaneous.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # DONE Adatta lo script al sistema ibisco
 # Procedura dalla lezione del 2023-05-12
+# TODO Assicurati di prendere POTCAR dal posto giusto
 if [ ! -f POTCAR ]; then
    cp ../00.PerfectCrystal/POTCAR .
 fi
@@ -27,7 +28,7 @@ for v in 17.0; do
 
    ln -s "$SLURM_SUBMIT_DIR/POTCAR" .
 
-cat >POSCAR <<EOF
+   cat >POSCAR <<EOF
 super cell
         -$vsuper
    2.000000000000000   0.000000000000000   2.000000000000000
@@ -101,7 +102,7 @@ super cell
    0.750000000000000   0.750000000000000   0.750000000000000
 EOF
 
-cat >SPOSCAR <<EOF
+   cat >SPOSCAR <<EOF
 super cell
         4.0000000000
    2.000000000000000   0.000000000000000   2.000000000000000
@@ -182,45 +183,43 @@ EOF
       cp POSCAR $folder
    done
 
-cat >01/INCAR <<EOF
+   cat >01/INCAR <<EOF
 XLAMBDA=0.0
 EOF
 
-cat >02/INCAR <<EOF
+   cat >02/INCAR <<EOF
 XLAMBDA=0.333333333
 EOF
 
-cat >03/INCAR <<EOF
+   cat >03/INCAR <<EOF
 XLAMBDA=0.666666666
 EOF
 
-cat >04/INCAR <<EOF
+   cat >04/INCAR <<EOF
 XLAMBDA=1.0
 EOF
 
-# Allo stato dei fatti, abbiamo una struttura con tutti gli atomi
-# nelle proprie posizioni di equilibrio. Avviando VASP con questa
-# configurazione, otterremmo un valore dell'energia corrispondente a
-# U_0. Dobbiamo configurare INCAR per abilitare la dinamica
-# molecolare. Il parametro per attivare la dinamica molecolare è
-# IBRION=0.
+   # Allo stato dei fatti, abbiamo una struttura con tutti gli atomi nelle
+   # proprie posizioni di equilibrio. Avviando VASP con questa configurazione,
+   # otterremmo un valore dell'energia corrispondente a U_0. Dobbiamo
+   # configurare INCAR per abilitare la dinamica molecolare. Il parametro per
+   # attivare la dinamica molecolare è IBRION=0.
 
-# TODO NSW dovrebbe essere 2000 per avere buoni risultati, ma ora sto
-# facendo un test, quindi lo imposto a 200.
+   # TODO NSW dovrebbe essere 2000 per avere buoni risultati, ma ora sto facendo
+   # un test, quindi lo imposto a un valore minore.
 
-# POTIM=3.0 è il passo temporale in femto secondi. TEBEG=600 è la
-# temperatura iniziale della simulazione.
+   # POTIM=3.0 è il passo temporale in femto secondi. TEBEG=600 è la temperatura
+   # iniziale della simulazione.
 
-# Usiamo EDIFF=1d-4 perché la convergenza sia meno stringente e più
-# rapida. IALGO=48 impiega un algoritmo di diagonalizzazione più
-# rapido ma anche più instabile. MAXMIX=40 utilizza più densità dai
-# passi precedenti, per velocizzare la convergenza.
+   # Usiamo EDIFF=1d-4 perché la convergenza sia meno stringente e più rapida.
+   # IALGO=48 impiega un algoritmo di diagonalizzazione più rapido ma anche più
+   # instabile. MAXMIX=40 utilizza più densità dai passi precedenti, per
+   # velocizzare la convergenza.
 
-# TIPO=inteharm indica che vogliamo effettuare l'integrazione
-# termodinamica con una combinazione di potenziale armonico e
-# potenziale completo.
+   # TIPO=inteharm indica che vogliamo effettuare l'integrazione termodinamica
+   # con una combinazione di potenziale armonico e potenziale completo.
 
-cat >INCAR.molecular-dynamics <<EOF
+   cat >INCAR.molecular-dynamics <<EOF
 EDIFF=1d-4
 SIGMA=0.544
 LWAVE=.FALSE.
@@ -256,10 +255,10 @@ XLAMBDA=0.0
 #LADIABATIC=.T.
 EOF
 
-# Dato che ci interessano solo le differenze rispetto al valore di
-# riferimento dell'energia, effettuiamo i calcoli solo in un punto.
+   # Dato che ci interessano solo le differenze rispetto al valore di
+   # riferimento dell'energia, effettuiamo i calcoli solo in un punto.
 
-cat >KPOINTS <<EOF
+   cat >KPOINTS <<EOF
 auto
  0
 Gamma
@@ -267,10 +266,11 @@ Gamma
 0 0 0
 EOF
 
-# We need the FORCES file to generate the HARMONIC file
-# TODO Crea flusso di creazione del file con runphon
+   # We need the FORCES file to generate the HARMONIC file
+   #
+   # TODO Crea flusso di creazione del file con runphon
 
-cat >INCAR.harmonic <<EOF
+   cat >INCAR.harmonic <<EOF
 EDIFF=1d-6
 SIGMA=0.544
 LWAVE=.FALSE.
@@ -279,30 +279,33 @@ LCHARG=.FALSE.
 NPAR=1
 EOF
 
-FILE_FORCES=FORCES.s$s.k$k.v$v
-if [ -f FORCES ]; then
-  rm FORCES
-fi
-if [ -f "$SLURM_SUBMIT_DIR/$FILE_FORCES" ]; then
-  echo "File $FILE_FORCES found"
-  ln -Ts "$SLURM_SUBMIT_DIR/$FILE_FORCES" FORCES
-else
-   echo "Running runphon to generate $FILE_FORCES"
-   cp INCAR.harmonic INCAR
-   $RUNPHON
-   cp FORCES "$SLURM_SUBMIT_DIR/$FILE_FORCES"
-fi
-unset FILE_FORCES
+   FILE_FORCES=FORCES.s$s.k$k.v$v
+   if [ -f FORCES ]; then
+      rm FORCES
+   fi
+   if [ -f "$SLURM_SUBMIT_DIR/$FILE_FORCES" ]; then
+      echo "File $FILE_FORCES found"
+      ln -Ts "$SLURM_SUBMIT_DIR/$FILE_FORCES" FORCES
+   else
+      echo "Running runphon to generate $FILE_FORCES"
+      cp INCAR.harmonic INCAR
+      $RUNPHON
+      cp FORCES "$SLURM_SUBMIT_DIR/$FILE_FORCES"
+   fi
+   unset FILE_FORCES
 
-# Remember to use the correct HARMONIC file force constant matrix and set the
-# correct radius TODO Creare il file HARMONIC adatto al volume, se non esiste
-# già Per far scrivere a PHON nel file HARMONIC la matrice delle costanti di
-# forza, impostare nel file INPHON le seguenti variabili:
-# LSUPER = .F., LFORCEOUT = .T.
+   # Remember to use the correct HARMONIC file force constant matrix and set the
+   # correct radius
+   #
+   # TODO Creare il file HARMONIC adatto al volume, se non esiste già
+   #
+   # Per far scrivere a PHON nel file HARMONIC la matrice delle costanti di
+   # forza, impostare nel file INPHON le seguenti variabili: LSUPER = .F.,
+   # LFORCEOUT = .T.
 
-if [ ! -f HARMONIC.v$v ]; then
-   echo "    HARMONIC.v$v not found; generating it with PHON"
-   cat >INPHON <<EOF
+   if [ ! -f HARMONIC.v$v ]; then
+      echo "[I] HARMONIC.v$v not found; generating it with PHON"
+      cat >INPHON <<EOF
 # number of ions types and masses
 NTYPES = 1
 MASS =26.98
@@ -339,11 +342,14 @@ DOSSMEAR = 0.05
 # verbosity
 IPRINT = 0
 EOF
-   $PHON
-fi
+      $PHON
+      cp HARMONIC HARMONIC.v$v
+   else
+      cp HARMONIC.v$v HARMONIC
+   fi
 
-cp HARMONIC.v$v HARMONIC
+   # Run VASP for molecular dynamics
+   cp INCAR.molecular-dynamics INCAR
+   $MPI -np 4 "$VASPGAMMA"
 
-# Run VASP
-cp INCAR.molecular-dynamics INCAR
-$MPI -np 4 "$VASPGAMMA"
+done # v
