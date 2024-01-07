aa088e78c1147896d6811546810abc4a3e130cb0
Mariano Mollo <marian.mollo@studenti.unina.it>
2024-01-08 00:29:20 +0100

    carica .dir-locals.el

---
+++ tutorial-mlp-water/.dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((ssh-deploy-root-local . "/home/mariano/Progetti/tesi-magistrale/tutorial-mlp-water/")
+         (ssh-deploy-root-remote . "/ssh:ibisco:/ibiscostorage/VirtualMatterLab/Mollo/tutorial-mlp-water/")
+         (ssh-deploy-on-explicit-save . nil))))
