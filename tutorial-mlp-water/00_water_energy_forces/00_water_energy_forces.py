36fb93d0dedc2ed7d23577e5aead16f1464bda98
Mariano Mollo <marian.mollo@studenti.unina.it>
2024-01-10 23:23:47 +0100

    Aggiungi descrizione e aggiusta formattazione

--- tutorial-mlp-water/00_water_energy_forces/00_water_energy_forces.py
+++ tutorial-mlp-water/00_water_energy_forces/00_water_energy_forces.py
@@ -1,17 +1,29 @@
+#!/usr/bin/env python
+
+# Ottieni energia e forze di una specifica geometria
+
 # ase I/O
-from ase.io import read, write
+from ase.io import read
+
 # LAMMPS calculator
 from ase.calculators.lammpslib import LAMMPSlib
 
 # Inizializza LAMMPS
-atom_types = {'O' : 2, 'H' : 1}
-cmd_nn = [      "#!/bin/bash",
-                "variable runnerDir       string \"/lustre/home/tccourse/ZEN/TEST/train_008\"",
-                "variable runnerCutoff    equal  8.466835984",
-                "pair_style hdnnp ${runnerCutoff} dir ${runnerDir} showew no showewsum 1 resetew yes maxew 200000 cflength 1.889726 cfenergy 0.036749",
-                "pair_coeff * * H O",
-        ]
-LAMMPScalc = LAMMPSlib(atom_types=atom_types, lmpcmds=cmd_nn, tmp_dir="./", log_file='log.lammps', keep_alive=True)
+atom_types = {"O": 2, "H": 1}
+cmd_nn = [
+    "#!/bin/bash",
+    'variable runnerDir       string "/lustre/home/tccourse/ZEN/TEST/train_008"',
+    "variable runnerCutoff    equal  8.466835984",
+    "pair_style hdnnp ${runnerCutoff} dir ${runnerDir} showew no showewsum 1 resetew yes maxew 200000 cflength 1.889726 cfenergy 0.036749",
+    "pair_coeff * * H O",
+]
+LAMMPScalc = LAMMPSlib(
+    atom_types=atom_types,
+    lmpcmds=cmd_nn,
+    tmp_dir="./",
+    log_file="log.lammps",
+    keep_alive=True,
+)
 
 # Load a geometry and compute its energy
 
@@ -25,4 +37,4 @@ energy = atoms.get_potential_energy()
 print("Energy:", energy)
 # Compute the forces on each atom
 forces = atoms.get_forces()
-print('Forces:\n', forces)
+print("Forces:\n", forces)
