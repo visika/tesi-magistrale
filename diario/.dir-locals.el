245caedc956acfca82197d37571f577c8cb55a31
Mariano Mollo <marianomollo@protonmail.ch>
2024-01-20 14:21:09 +0100

    carica dir-locals

---
+++ diario/.dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((ssh-deploy-root-local . "/home/mariano/Progetti/tesi-magistrale/diario/")
+         (ssh-deploy-root-remote . "/ssh:ibisco:/ibiscostorage/mmollo/diario/")
+         (ssh-deploy-on-explicit-save . nil))))
