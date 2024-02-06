a7b997040df1a7c2bf2082bcd7c4b58069f4902a
Mariano Mollo <m.mollo@sintechsolution.it>
2024-02-06 01:00:57 +0100

    aggiorna notebook

--- 02_water/02_dimer/01_lammps/01.1_angles.py
+++ 02_water/02_dimer/01_lammps/01.1_angles.py
@@ -6,13 +6,14 @@ app = marimo.App()
 
 @app.cell
 def __():
+    import marimo as mo
     from ase.io import read
-    return read,
+    return mo, read
 
 
 @app.cell
 def __(read):
-    atoms = read("final.pdb")
+    atoms = read("01_relax-positions/final.pdb")
     return atoms,
 
 
@@ -28,47 +29,56 @@ def __(atoms, view):
     return
 
 
-@app.cell
-def __():
-    # 0 ossigeno accettore
-    # 1 idrogeno accettore
-    # 2 idrogeno accettore
-    # 3 ossigeno donore
-    # 5 idrogeno che punta
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(
+        """
+        Ottieni gli indici selezionando la voce di menu:
+        `View > Show Labels > Atom Index`
+
+        - 0 ossigeno accettore
+        - 1 idrogeno accettore
+        - 2 idrogeno accettore
+        - 3 ossigeno donore
+        - 4 idrogeno donore esterno (f)
+        - 5 idrogeno donore che punta (d)
+        """
+    )
     return
 
 
 @app.cell
 def __(atoms):
-    # alpha = 5.5°
-    atoms.get_angle(0, 3, 5)
+    # Angolo alpha = 5.5°
+    atoms.get_angle(0, 3, 5).round(1)
     return
 
 
 @app.cell
 def __(atoms):
-    # Molecola accettore
-    atoms.get_angle(1, 0, 2)
+    # Angolo interno della molecola accettore = 104.87°
+    atoms.get_angle(1, 0, 2).round(2)
+    return
+
+
+@app.cell
+def __(atoms):
+    # Angolo interno della molecola donore = 104.83°
+    atoms.get_angle(4, 3, 5).round(2)
     return
 
 
 @app.cell
 def __(atoms):
     # Distanza O-O = 291.2 pm
-    atoms.get_distance(0, 3)
+    atoms.get_distance(0, 3).round(2)
     return
 
 
 @app.cell
 def __(atoms):
-    # beta = 124.4°
-    atoms.get_angle(1, 0, 3)
-    return
-
-
-@app.cell
-def __(atoms):
-    atoms.get_dihedral(0, 1, 3, 0)
+    # Angolo beta = 124.4°
+    atoms.get_angle(1, 0, 3).round(1)
     return
 
 
