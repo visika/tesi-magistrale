48e2d79f1c81c311b8ccb6826066619d5fd37489
Mariano Mollo <m.mollo@sintechsolution.it>
2024-03-06 02:32:10 +0100

    aggiorna analisi binding energy

--- 02_water/02_dimer/02_binding_energy/analysis.py
+++ 02_water/02_dimer/02_binding_energy/analysis.py
@@ -1,7 +1,21 @@
 import marimo
 
-__generated_with = "0.2.5"
+__generated_with = "0.2.13"
 app = marimo.App()
+
+
+@app.cell
+def __(mo):
+    mo.md(
+        """
+        # Binding energy
+
+        Il modello è MACE-MP-0 medium con dispersione.
+
+        L'altro è n2p2.
+        """
+    )
+    return
 
 
 @app.cell
@@ -12,13 +26,11 @@ def __():
 
 @app.cell
 def __(pd):
-    df_1 = pd.read_csv('energies.csv')
-    df_2 = pd.read_csv('energies_2.csv')
-    df = pd.concat([df_1, df_2])
+    df = pd.read_csv('MACE-MP-0/energies.csv')
     # Sort the dataframe according to distance
     df = df.sort_values(by='distance')
     df
-    return df, df_1, df_2
+    return df,
 
 
 @app.cell
@@ -29,8 +41,15 @@ def __(df):
 
 @app.cell
 def __():
-    e_gas_ev = -14.170072284496875
-    return e_gas_ev,
+    # Read value from file
+    f = open(
+        "../../01_molecule/01.8_molecule_converge_fmax_parallel_dispersion/medium/1e-8/e_gas.txt",
+        "r",
+    )
+    e_gas_ev = float(f.read())
+    # e_gas_ev = -14.170072284496875
+    e_gas_ev
+    return e_gas_ev, f
 
 
 @app.cell(hide_code=True)
@@ -52,20 +71,38 @@ def __():
 def __(df, e_gas_ev):
     # Calculate the binding energy as a new column
     df['binding_energy'] = df['energy'] - 2 * e_gas_ev
-    df.plot(x="distance", y="binding_energy", ylim=(-0.4, 0.1))
+    df.plot(x="distance", y="binding_energy", ylim=(-0.4, 0.1), xlim=(2, 7))
     return
 
 
 @app.cell
 def __(df):
     df["binding_energy_kjmol"] = df["binding_energy"] * 96.4916
-    df.plot(x="distance", y="binding_energy_kjmol", ylim=(-35, 0), xlabel="$r_{OO}$")
+    df.plot(
+        x="distance",
+        y="binding_energy_kjmol",
+        ylim=(-35, 0),
+        xlabel="$r_{OO}$",
+        xlim=(2, 7),
+    )
     return
 
 
 @app.cell
-def __():
-    return
+def __(pd):
+    df_n2p2 = pd.read_csv('n2p2/energies.csv')
+    df_n2p2 = df_n2p2.sort_values(by='distance')
+    df_n2p2.plot(x="distance", y="energy")
+    e_gas_n2p2_ev = -468.46641416393885
+    df_n2p2['binding_energy'] = df_n2p2['energy'] - 2 * e_gas_n2p2_ev
+    df_n2p2["binding_energy_kjmol"] = df_n2p2["binding_energy"] * 96.4916
+    df_n2p2.plot(
+        x="distance",
+        y="binding_energy_kjmol",
+        xlabel="$r_{OO}$",
+        xlim=(2, 7),
+    )
+    return df_n2p2, e_gas_n2p2_ev
 
 
 if __name__ == "__main__":
