36dd516f3a9f7608894af2a23fb1c85f1fc0ab7e
Mariano Mollo <m.mollo@sintechsolution.it>
2024-03-07 16:13:55 +0100

    racchiudi plot in una funzione

--- simulazioni/02_water/04_crystal_phonons/MACE-MP-0 medium/Ih/script.py
+++ simulazioni/02_water/04_crystal_phonons/MACE-MP-0 medium/Ih/script.py
@@ -13,6 +13,32 @@ def optimize(a, fmax: float, output_path: str) -> None:
     opt.run(fmax=fmax, steps=1000)
     write(images=atoms, filename=output_path + "/final.xyz")
 
+def plot_the_figure() -> None:
+    fig = plt.figure(1, figsize=(7, 5))
+    ax = fig.add_axes([0.12, 0.07, 0.67, 0.85])
+    plt.title(
+        f"MACE-MP-0-d3-medium: ICE Ih${N}^3$ supercell, ${K}^3$ k-points $\\delta = ${delta}, fmax={fmax}"
+    )
+    emin = -0.04
+    emax = 0.06
+    bandstructure.plot(ax=ax, emin=emin, emax=emax)
+    dosax = fig.add_axes([0.8, 0.07, 0.17, 0.85])
+    dosax.fill_between(
+        dos.get_weights(),
+        dos.get_energies(),
+        y2=0,
+        color="grey",
+        edgecolor="k",
+        lw=1,
+    )
+    dosax.set_ylim(emin, emax)
+    dosax.set_yticks([])
+    dosax.set_xticks([])
+    dosax.set_xlabel("DOS", fontsize=16)
+    plt.savefig(f"bandstructure_delta={delta}_N={N}.png")
+    plt.close(fig)
+
+
 fmax = 1e-8
 
 atoms = read("POSCAR")
@@ -40,27 +66,4 @@ bandpath = atoms.cell.bandpath(npoints=100)
 bandstructure = ph.get_band_structure(bandpath)
 dos = ph.get_dos(kpts=(K, K, K)).sample_grid(npts=100, width=1e-3)
 
-# Plot the figure
-fig = plt.figure(1, figsize=(7, 5))
-ax = fig.add_axes([0.12, 0.07, 0.67, 0.85])
-plt.title(
-    f"MACE-MP-0 medium D: ICE Ih${N}^3$ supercell, ${K}^3$ k-points $\\delta = {delta}$, fmax={fmax}"
-)
-emin = -0.04
-emax = 0.06
-bandstructure.plot(ax=ax, emin=emin, emax=emax)
-dosax = fig.add_axes([0.8, 0.07, 0.17, 0.85])
-dosax.fill_between(
-    dos.get_weights(),
-    dos.get_energies(),
-    y2=0,
-    color="grey",
-    edgecolor="k",
-    lw=1,
-)
-dosax.set_ylim(emin, emax)
-dosax.set_yticks([])
-dosax.set_xticks([])
-dosax.set_xlabel("DOS", fontsize=16)
-plt.savefig(f"bandstructure_delta={delta}.png")
-plt.close(fig)
+plot_the_figure()
