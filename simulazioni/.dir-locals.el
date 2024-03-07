54484e53707cd5bf89f1533dd7e53a4c0275f6b6
Mariano Mollo <m.mollo@sintechsolution.it>
2024-03-07 01:09:41 +0100

    ricollega a ibisco

---
+++ simulazioni/.dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((ssh-deploy-root-local . "/home/mariano/Progetti/tesi-magistrale/simulazioni/")
+         (ssh-deploy-root-remote . "/ssh:ibisco:/ibiscostorage/mmollo/tesi-magistrale/")
+         (ssh-deploy-on-explicit-save . nil))))
