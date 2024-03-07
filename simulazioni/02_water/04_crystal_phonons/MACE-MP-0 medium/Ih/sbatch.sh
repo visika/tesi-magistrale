75295d1620d08aca713eb09925e52d697820167c
Mariano Mollo <m.mollo@sintechsolution.it>
2024-03-07 16:17:41 +0100

    abbandona conda maledetto abbraccia la poesia

--- simulazioni/02_water/04_crystal_phonons/MACE-MP-0 medium/Ih/sbatch.sh
+++ simulazioni/02_water/04_crystal_phonons/MACE-MP-0 medium/Ih/sbatch.sh
@@ -19,24 +19,11 @@ cd "$SLURM_SUBMIT_DIR" || exit
 ALERT=000-THIS_IS_RUNNING
 touch $ALERT
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/lustre/home/mmollo/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/lustre/home/mmollo/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/lustre/home/mmollo/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/lustre/home/mmollo/miniconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
-
-conda activate mace_env
+source $(poetry env info --path)/bin/activate
 
 echo Using "$(python -V)"
+echo Python path: "$(which python)"
+
 python script.py
 
 date
