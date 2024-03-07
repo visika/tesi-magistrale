c9b2e9b4a492ff68400c989f9cac3b159ec4db08
Mariano Mollo <m.mollo@sintechsolution.it>
2024-03-07 16:18:29 +0100

    ristruttura cartelle su ibisco e aggiusta variabili

--- simulazioni/02_water/.dir-locals.el
+++
@@ -1,3 +0,0 @@
-((nil . ((ssh-deploy-root-local . "/home/mariano/Progetti/tesi-magistrale/02_water/")
-         (ssh-deploy-root-remote . "/ssh:ibisco:/ibiscostorage/mmollo/02_water/")
-         (ssh-deploy-on-explicit-save . nil))))
