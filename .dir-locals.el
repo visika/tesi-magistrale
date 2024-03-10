c0813e0e61433ea74a60c7cb8940d9ab864d385d
Mariano Mollo <m.mollo@sintechsolution.it>
2024-03-10 19:32:37 +0100

    togli percorso locale da .dir-locals
    così prende sempre il percorso corretto della repository quando cambio
    computer

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,5 @@
 ((nil . (
          ;; Impostazione per far leggere il PATH di ibisco a vterm
          (eval . (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
-         (ssh-deploy-root-local . "/home/mariano/Progetti/tesi-magistrale/simulazioni/")
          (ssh-deploy-root-remote . "/ssh:ibisco:/ibiscostorage/mmollo/tesi-magistrale/")
          (ssh-deploy-on-explicit-save . nil))))
