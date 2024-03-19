4db9cbc3795d82d3693e3caf7c18856da5c0e8a9
Mariano Mollo <m.mollo@sintechsolution.it>
2024-03-19 01:56:51 +0100

    Carica simulazione
    Ho il dubbio che la dispersione non sia stata attivata.
    Devo riprovare usando mace_mp.

---
+++ simulazioni/02_water/03_ICE13_lattice_energies/MACE-ICE13-1-D3/script.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import datetime
+import pandas as pd
+from ase.io import read
+from mace.calculators.mace import MACECalculator
+
+now = datetime.datetime.now()
+
+t = now
+print(f"Start python script at {t}")
+
+structures = [
+    "Ih",
+    "II",
+    "III",
+    "IV",
+    "VI",
+    "VII",
+    "VIII",
+    "IX",
+    "XI",
+    "XIII",
+    "XIV",
+    "XV",
+    "XVII",
+]
+
+crystal_energies = []
+
+calc = MACECalculator(
+    "/ibiscostorage/VirtualMatterLab/MACE-ICE13/MACE-ICE13-1.model",
+    dispersion=True,
+    default_dtype="float64",
+    device="cuda",
+)
+
+for s in structures:
+    atoms = read(f"/ibiscostorage/mmollo/tesi-magistrale/strutture/ICE13/{s}/POSCAR")
+    atoms.calc = calc
+    nmol = atoms.get_global_number_of_atoms() / 3.0
+    energy = atoms.get_potential_energy()
+    e_crys = energy / nmol
+    crystal_energies.append(e_crys)
+
+dict = {"structure": structures, "e_crys": crystal_energies}
+df = pd.DataFrame(data=dict)
+df.to_csv("crystal_energies.csv", index=False)
+
+dt = now - t
+print(f"Finished python script after {dt}")
