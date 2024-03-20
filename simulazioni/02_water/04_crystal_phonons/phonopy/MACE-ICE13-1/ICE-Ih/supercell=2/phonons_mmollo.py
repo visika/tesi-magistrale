5b1111e911aa1efa564afcc6eaf2c0dd117c5123
Mariano Mollo <m.mollo@sintechsolution.it>
2024-03-20 01:59:33 +0100

    carica run fononi con mace-ice13-1

---
+++ simulazioni/02_water/04_crystal_phonons/phonopy/MACE-ICE13-1/ICE-Ih/supercell=2/phonons_mmollo.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+import phonopy
+from ase import Atoms
+from datetime import datetime
+from ase.optimize import BFGS
+from ase.io import read, write
+from mace.calculators.mace import MACECalculator
+from phonopy.structure.atoms import PhonopyAtoms
+
+
+def optimize(a: Atoms, fmax: float, output: str = "optimized.xyz") -> None:
+    opt = BFGS(a, trajectory="optimization.traj", logfile="optimization.log")
+    opt.run(fmax=fmax)
+    write(filename=output, images=a)
+
+
+def ASE2PhonopyAtoms(s: Atoms) -> PhonopyAtoms:
+    return PhonopyAtoms(
+        symbols=s.get_chemical_symbols(),
+        cell=s.cell.array,
+        scaled_positions=s.get_scaled_positions(),
+        masses=s.get_masses(),
+    )
+
+
+def calc_forces(calculator, s: PhonopyAtoms):
+    atoms = Phonopy2ASEAtoms(s)
+    atoms.calc = calculator
+    return atoms.get_forces()
+
+
+def Phonopy2ASEAtoms(s: PhonopyAtoms) -> Atoms:
+    return Atoms(
+        symbols=s.symbols,
+        scaled_positions=s.scaled_positions,
+        cell=s.cell,
+        masses=s.masses,
+        pbc=True,
+    )
+
+
+atoms = read(filename="/ibiscostorage/mmollo/tesi-magistrale/strutture/ICE13/Ih/POSCAR")
+
+calculator = MACECalculator(
+    "/ibiscostorage/VirtualMatterLab/MACE-ICE13/MACE-ICE13-1.model",
+    default_dtype="float64",
+    device="cuda",
+)
+
+atoms.calc = calculator
+
+t = datetime.now()
+print(f"Optimization started at {t}")
+optimize(atoms, fmax=1e-8)
+dt = datetime.now() - t
+print(f"Optimization finished after {dt}")
+
+cell = ASE2PhonopyAtoms(atoms)
+
+t = datetime.now()
+print(f"Displacements started at {t}")
+nc = 2
+supercell_matrix = ((nc, 0, 0), (0, nc, 0), (0, 0, nc))
+ph = phonopy.Phonopy(cell, supercell_matrix)
+ph.generate_displacements(distance=0.01)
+disp_supercells = ph.supercells_with_displacements
+dt = datetime.now() - t
+print(f"Displacements finished after {dt}")
+
+t = datetime.now()
+print(f"Forces calculation started at {t}")
+ph.forces = [
+    calc_forces(calculator, supercell)
+    for supercell in disp_supercells
+    if supercell is not None
+]
+dt = datetime.now() - t
+print(f"Forces calculation finished after {dt}")
+
+t = datetime.now()
+print(f"Force constants calculation started at {t}")
+ph.produce_force_constants()
+dt = datetime.now() - t
+print(f"Force constants calculation finished after {dt}")
+
+ph.save("phonopy_params.yaml", settings={"force_constants": True})
+print("Goodbye from Python!")
---
+++ simulazioni/02_water/04_crystal_phonons/phonopy/MACE-ICE13-1/ICE-Ih/supercell=2/sbatch.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+#SBATCH --job-name=Phonons
+#SBATCH --partition=gpus
+#SBATCH --ntasks=1
+#SBATCH --mem=4G
+#SBATCH --gpus=1
+#SBATCH --propagate=NONE
+
+echo Starging at $(date)
+
+ulimit -s unlimited
+
+source /nfsexports/SOFTWARE/intel/oneapi/setvars.sh
+export MPI="mpirun"
+echo "     using $(which $MPI)"
+
+cd "$SLURM_SUBMIT_DIR" || exit
+
+ALERT=000-THIS_IS_RUNNING
+touch $ALERT
+
+source $(poetry env info --path)/bin/activate
+
+echo Using "$(python -V)"
+echo Python path: "$(which python)"
+
+python3 phonons_mmollo.py
+
+date
+echo "FINISHED!!!"
+rm $ALERT
