325851b15081a8013841e05d7074f04e5172febf
Mariano Mollo <m.mollo@sintechsolution.it>
2024-04-01 18:06:30 +0200

    rimuovi riga che da fastidio a tramp
    vterm in ssh lasciamolo stare per ora

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ((nil . (
          ;; Impostazione per far leggere il PATH di ibisco a vterm
-         (eval . (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
+         ;; (eval . (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
          (ssh-deploy-root-remote . "/ssh:ibisco:/ibiscostorage/mmollo/tesi-magistrale/")
          (ssh-deploy-on-explicit-save . nil))))
