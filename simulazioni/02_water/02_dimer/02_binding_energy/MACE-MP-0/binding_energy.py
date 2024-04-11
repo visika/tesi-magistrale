4add79b8a68226eca6903d772b15300e5da7a1c8
Mariano Mollo <m.mollo@sintechsolution.it>
2024-04-11 22:49:05 +0200

    aggiorna script e genera nuovi dati

--- simulazioni/02_water/02_dimer/02_binding_energy/MACE-MP-0/binding_energy.py
+++ simulazioni/02_water/02_dimer/02_binding_energy/MACE-MP-0/binding_energy.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 import numpy as np
 import pandas as pd
 from ase.io import read, write
@@ -11,33 +12,39 @@ atoms = read("init.xyz")
 
 calc = mace_mp(model="medium", dispersion=True, default_dtype="float64", device="cuda")
 
+# Costruisco un array nel quale conservare tutte le configurazioni di atomi
+# posti a distanze differenti tra loro
 atoms_array = []
 distance_oo = atoms.get_distance(0, 3)
 vector_oo = atoms.get_distance(0, 3, mic=True, vector=True)
 direction_oo = vector_oo / distance_oo
-distances = np.linspace(2.0, 2.7, 8)
+# Definisco l'intervallo di distanze tra i due atomi di ossigeno che voglio
+# campionare. Qualcosa come un punto ogni 0.1 Å può andare bene.
+distances = np.linspace(2.0, 6.0, num=40)
 displacements = distances - distance_oo
 potentialenergies = []
 
-for d, dist in zip(displacements, distances):
+os.makedirs("geometries", exist_ok=True)
+os.makedirs("optimization", exist_ok=True)
+for disp, dist in zip(displacements, distances):
     atoms_distanced = atoms.copy()
-    atoms_distanced[3].position += direction_oo * d
-    atoms_distanced[4].position += direction_oo * d
-    atoms_distanced[5].position += direction_oo * d
+    atoms_distanced[3].position += direction_oo * disp
+    atoms_distanced[4].position += direction_oo * disp
+    atoms_distanced[5].position += direction_oo * disp
     constraint = FixBondLength(0, 3)
     atoms_distanced.set_constraint(constraint)
     atoms_distanced.calc = calc
     opt = BFGS(
         atoms_distanced,
-        logfile=f"optimization_d={d}.log",
-        trajectory=f"optimization_d={d}.traj",
+        logfile=f"optimization/dist={dist}.log",
+        trajectory=f"optimization/dist={dist}.traj",
     )
     opt.run(steps=100)
-    write(f"final_d={dist}.xyz", atoms_distanced)
+    write(f"geometries/final_dist={dist}.xyz", atoms_distanced)
     potentialenergy = atoms_distanced.get_potential_energy()
     potentialenergies.append(potentialenergy)
     atoms_array.append(atoms_distanced)
 
 dict = {"distance": distances, "energy": potentialenergies}
 df = pd.DataFrame(data=dict)
-df.to_csv("energies_2.csv", index=False)
+df.to_csv("energies.csv", index=False)
