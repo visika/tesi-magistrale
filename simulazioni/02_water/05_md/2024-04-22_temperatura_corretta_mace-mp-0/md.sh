8f7b6679f32c919b713f5045024d7c69019499f5
Mariano Mollo <m.mollo@sintechsolution.it>
2024-04-22 23:08:31 +0200

    carica run

---
+++ simulazioni/02_water/05_md/2024-04-22_temperatura_corretta_mace-mp-0/md.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=MDNVT
+#SBATCH --partition=gpus
+#SBATCH --ntasks=1
+#SBATCH --mem=4G
+#SBATCH --gpus=1
+#SBATCH --propagate=NONE
+
+ulimit -s unlimited
+
+source /nfsexports/SOFTWARE/intel/oneapi/setvars.sh
+export MPI="mpirun"
+echo "     using $(which $MPI)"
+
+# Questo serve per poetry
+export PATH=$PATH:/lustre/home/mmollo/.local/bin
+
+cd "$SLURM_SUBMIT_DIR" || exit
+
+ALERT=000-THIS_IS_RUNNING
+touch $ALERT
+
+source "$(poetry env info --path)/bin/activate"
+
+echo Using "$(python -V)"
+echo Python path: "$(which python)"
+
+python md.py
+
+echo "FINISHED!!!"
+rm $ALERT
