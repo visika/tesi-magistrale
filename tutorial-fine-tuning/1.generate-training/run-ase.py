b8abfc8cea1e2d75ed28cbaa4bfd03e7940f9511
Mariano Mollo <marianomollo@protonmail.ch>
2024-06-08 21:54:24 +0200

    imposta dinamica NPT

--- tutorial-fine-tuning/1.generate-training/run-ase.py
+++ tutorial-fine-tuning/1.generate-training/run-ase.py
@@ -3,7 +3,8 @@ from ase.io import read
 from datetime import datetime
 
 # from mace.calculators.mace import MACECalculator
-from ase.md.langevin import Langevin  # NVT molecular dynamics
+# from ase.md.langevin import Langevin  # NVT molecular dynamics
+from ase.md.nptberendsen import NPTBerendsen
 from ase.md import MDLogger
 import ase.units as units
 from mace.calculators import mace_mp
@@ -13,18 +14,32 @@ mace = mace_mp()
 atoms = read("POSCAR", 0)
 atoms.calc = mace
 
+# dyn = Langevin(
+#     atoms,
+#     timestep=0.5 * units.fs,
+#     temperature_K=100.0,  # temperature in K
+#     friction=0.01 / units.fs,
+#     trajectory="md.traj",
+# )
 
-dyn = Langevin(
+print("Setting dynamics")
+dyn = NPTBerendsen(
     atoms,
     timestep=0.5 * units.fs,
-    temperature_K=100.0,  # temperature in K
-    friction=0.01 / units.fs,
+    temperature_K=100.0,
+    pressure_au=1.01325 * units.bar,
+    # Compressibility depends on temperature
+    compressibility_au=0.9471 * 1e-10 / units.Pascal,
+    taut=100 * units.fs,
+    taup=500 * units.fs,
     trajectory="md.traj",
 )
+# 0.5 ps = 500 fs
 
 dyn.attach(
     MDLogger(dyn, atoms, "md.log", header=True, stress=False, mode="a"), interval=4
 )
+print("Finished setting dynamics")
 t = datetime.now()
 print(f"Starting dynamics at {t}")
 dyn.run(10000)
