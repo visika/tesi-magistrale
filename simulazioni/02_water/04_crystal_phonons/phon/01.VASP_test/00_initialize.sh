9d0d2c5a0eef2067e5d97f6b830c440781ca43ce
Mariano Mollo <marianomollo@protonmail.ch>
2024-09-29 22:22:24 +0200

    aggiungi run

---
+++ simulazioni/02_water/04_crystal_phonons/phon/01.VASP_test/00_initialize.sh
@@ -0,0 +1,6 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/nfsexports/SOFTWARE/nvidia/hpc_sdk/Linux_x86_64/22.3/comm_libs/openmpi4/openmpi-4.0.5/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/nfsexports/SOFTWARE/nvidia/hpc_sdk/Linux_x86_64/22.3/compilers/extras/qd/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/nfsexports/SOFTWARE/nvidia/hpc_sdk/Linux_x86_64/22.3/compilers/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/nfsexports/SOFTWARE/intel/oneapi/compiler/latest/linux/compiler/lib/intel64_lin
+
+export vasp_std=/ibiscostorage/VirtualMatterLab/vasp_6.3.0_bin/vasp_std
